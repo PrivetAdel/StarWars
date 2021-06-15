f94a8f3de01ee40ed1452c544e1791b5c8b80cea
Adel <rizvanovaadel@gmail.com>
2021-06-15 19:25:03 +0300

    deploy

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider>
-        <Router>
+        <Router basename='/star-wars/'>
           <App />
         </Router>
       </ThemeProvider>
